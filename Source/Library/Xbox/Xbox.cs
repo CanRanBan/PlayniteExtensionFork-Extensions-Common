2b33e7214cc0a8dcb473db29241bddd0515954cf
CanRanBan <canranban@gmx.de>
2023-11-19 09:38:01 +0100

    Move resources into own folder

--- Source/Library/Xbox/Xbox.cs
+++ Source/Library/Xbox/Xbox.cs
@@ -55,6 +55,6 @@ namespace XboxLibrary
             }
         }
 
-        public static string Icon { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"icon.png");
+        public static string Icon { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources", @"XboxLibraryIcon.png");
     }
 }
