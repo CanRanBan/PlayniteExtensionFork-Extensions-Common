0f852a0348a1310663452488b3147ba4396078b6
CanRanBan <canranban@gmx.de>
2023-11-19 13:20:11 +0100

    Use .ico instead of .png for extension icon

--- Source/Library/Xbox/Xbox.cs
+++ Source/Library/Xbox/Xbox.cs
@@ -55,6 +55,6 @@ namespace XboxLibrary
             }
         }
 
-        public static string Icon { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources", @"XboxLibraryIcon.png");
+        public static string Icon { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources", @"XboxLibraryIcon.ico");
     }
 }
