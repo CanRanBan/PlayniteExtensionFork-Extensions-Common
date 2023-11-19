f883907fb2259241ee2a869bb928240e10a67589
CanRanBan <canranban@gmx.de>
2023-11-19 03:04:56 +0100

    Use self-explanatory name for library icon

--- Source/Library/Battle.net/BattleNet.cs
+++ Source/Library/Battle.net/BattleNet.cs
@@ -59,7 +59,7 @@ namespace BattleNetLibrary
             }
         }
 
-        public static string Icon => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\battleneticon.png");
+        public static string Icon => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources", @"BattleNetLibraryIcon.png");
 
         public static void StartClient()
         {
